035cfb8d597d46a1a37ca1754e14cc506ec6e23a
ganymedes01 <foka_12@hotmail.com>
2015-05-01 21:03:30 -0300

    Fix Coarse Dirt not letting plants be placed on top of itself

--- src/main/java/ganymedes01/etfuturum/blocks/CoarseDirt.java
+++ src/main/java/ganymedes01/etfuturum/blocks/CoarseDirt.java
@@ -5,6 +5,10 @@ import ganymedes01.etfuturum.IConfigurable;
 import ganymedes01.etfuturum.core.utils.Utils;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.init.Blocks;
+import net.minecraft.world.IBlockAccess;
+import net.minecraftforge.common.IPlantable;
+import net.minecraftforge.common.util.ForgeDirection;
 
 public class CoarseDirt extends Block implements IConfigurable {
 
@@ -15,6 +19,11 @@ public class CoarseDirt extends Block implements IConfigurable {
 		setBlockTextureName("coarse_dirt");
 		setBlockName(Utils.getUnlocalisedName("coarse_dirt"));
 		setCreativeTab(EtFuturum.enableCoarseDirt ? EtFuturum.creativeTab : null);
+	}
+
+	@Override
+	public boolean canSustainPlant(IBlockAccess world, int x, int y, int z, ForgeDirection direction, IPlantable plant) {
+		return Blocks.dirt.canSustainPlant(world, x, y, z, direction, plant);
 	}
 
 	@Override
