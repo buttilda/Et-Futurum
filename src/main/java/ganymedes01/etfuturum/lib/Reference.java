fe750b8f71b95265b7ffac9fec60049af8d5160c
ganymedes01 <foka_12@hotmail.com>
2015-06-13 16:20:20 -0300

    Use latest recommended forge

--- src/main/java/ganymedes01/etfuturum/lib/Reference.java
+++ src/main/java/ganymedes01/etfuturum/lib/Reference.java
@@ -4,7 +4,7 @@ public class Reference {
 
 	public static final String MOD_ID = "etfuturum";
 	public static final String MOD_NAME = "Et Futurum";
-	public static final String DEPENDENCIES = "required-after:Forge@[10.13.2.1291,);";
+	public static final String DEPENDENCIES = "required-after:Forge@[10.13.4.1448,);";
 	public static final String VERSION_NUMBER = "1.1.1";
 	public static final String ITEM_BLOCK_TEXTURE_PATH = MOD_ID + ":";
 	public static final String ARMOUR_TEXTURE_PATH = ITEM_BLOCK_TEXTURE_PATH + "textures/models/armor/";
