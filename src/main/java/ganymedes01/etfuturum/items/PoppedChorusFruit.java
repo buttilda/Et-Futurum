040d8a4bc0373b4d03188fbad2648d219ab3397e
ganymedes01 <foka_12@hotmail.com>
2015-07-29 16:47:12 +0100

    Popped Chorus Fruit is not a food

--- src/main/java/ganymedes01/etfuturum/items/PoppedChorusFruit.java
+++ src/main/java/ganymedes01/etfuturum/items/PoppedChorusFruit.java
@@ -3,12 +3,11 @@ package ganymedes01.etfuturum.items;
 import ganymedes01.etfuturum.EtFuturum;
 import ganymedes01.etfuturum.IConfigurable;
 import ganymedes01.etfuturum.core.utils.Utils;
-import net.minecraft.item.ItemFood;
+import net.minecraft.item.Item;
 
-public class PoppedChorusFruit extends ItemFood implements IConfigurable {
+public class PoppedChorusFruit extends Item implements IConfigurable {
 
 	public PoppedChorusFruit() {
-		super(1, 0.6F, false);
 		setTextureName("chorus_fruit_popped");
 		setUnlocalizedName(Utils.getUnlocalisedName("chorus_fruit_popped"));
 		setCreativeTab(EtFuturum.enableChorusFruit ? EtFuturum.creativeTab : null);
