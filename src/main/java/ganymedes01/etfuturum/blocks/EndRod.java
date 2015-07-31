2eb215450522b9a81ac2d254d0e375096c9284e4
ganymedes01 <foka_12@hotmail.com>
2015-07-31 02:46:07 +0100

    End Rod block bounds are now set correctly (rendering is still completely broken)

--- src/main/java/ganymedes01/etfuturum/blocks/EndRod.java
+++ src/main/java/ganymedes01/etfuturum/blocks/EndRod.java
@@ -6,17 +6,32 @@ import ganymedes01.etfuturum.core.utils.Utils;
 import ganymedes01.etfuturum.lib.RenderIDs;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+import net.minecraftforge.common.util.ForgeDirection;
 
 public class EndRod extends Block implements IConfigurable {
 
 	public EndRod() {
 		super(Material.rock);
+		setHardness(0);
 		setLightLevel(0.9375F);
 		setBlockTextureName("end_rod");
 		setBlockName(Utils.getUnlocalisedName("end_rod"));
-		setBlockBounds(0.0F, 0.0F, 0.0F, 1.0F, 0.5F, 1.0F);
+		setBlockBounds(0.375F, 0.0F, 0.375F, 0.625F, 1.0F, 0.625F);
 		setCreativeTab(EtFuturum.enableChorusFruit ? EtFuturum.creativeTab : null);
+	}
+
+	@Override
+	public void setBlockBoundsBasedOnState(IBlockAccess world, int x, int y, int z) {
+		ForgeDirection dir = ForgeDirection.getOrientation(world.getBlockMetadata(x, y, z));
+
+		if (dir == ForgeDirection.DOWN || dir == ForgeDirection.UP)
+			setBlockBounds(0.375F, 0.0F, 0.375F, 0.625F, 1.0F, 0.625F);
+		else if (dir == ForgeDirection.WEST || dir == ForgeDirection.EAST)
+			setBlockBounds(0.0F, 0.375F, 0.375F, 1.0F, 0.625F, 0.625F);
+		else if (dir == ForgeDirection.NORTH || dir == ForgeDirection.SOUTH)
+			setBlockBounds(0.375F, 0.375F, 0.0F, 0.625F, 0.625F, 1.0F);
 	}
 
 	@Override
