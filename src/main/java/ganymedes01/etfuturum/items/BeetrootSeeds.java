b2ae65acb44edbebe7e3fb2286c6197c904f72da
ganymedes01 <foka_12@hotmail.com>
2015-08-02 02:26:10 +0100

    Beetroot seeds are found in dungeon loot

--- src/main/java/ganymedes01/etfuturum/items/BeetrootSeeds.java
+++ src/main/java/ganymedes01/etfuturum/items/BeetrootSeeds.java
@@ -6,6 +6,9 @@ import ganymedes01.etfuturum.ModBlocks;
 import ganymedes01.etfuturum.core.utils.Utils;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemSeeds;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.WeightedRandomChestContent;
+import net.minecraftforge.common.ChestGenHooks;
 
 public class BeetrootSeeds extends ItemSeeds implements IConfigurable {
 
@@ -14,6 +17,16 @@ public class BeetrootSeeds extends ItemSeeds implements IConfigurable {
 		setTextureName("beetroot_seeds");
 		setUnlocalizedName(Utils.getUnlocalisedName("beetroot_seeds"));
 		setCreativeTab(EtFuturum.enableBeetroot ? EtFuturum.creativeTab : null);
+
+		if (EtFuturum.enableBeetroot) {
+			ChestGenHooks.addItem(ChestGenHooks.MINESHAFT_CORRIDOR, new WeightedRandomChestContent(new ItemStack(this), 1, 2, 5));
+			ChestGenHooks.addItem(ChestGenHooks.PYRAMID_DESERT_CHEST, new WeightedRandomChestContent(new ItemStack(this), 1, 2, 5));
+			ChestGenHooks.addItem(ChestGenHooks.PYRAMID_JUNGLE_CHEST, new WeightedRandomChestContent(new ItemStack(this), 1, 2, 5));
+			ChestGenHooks.addItem(ChestGenHooks.STRONGHOLD_CORRIDOR, new WeightedRandomChestContent(new ItemStack(this), 1, 2, 5));
+			ChestGenHooks.addItem(ChestGenHooks.STRONGHOLD_LIBRARY, new WeightedRandomChestContent(new ItemStack(this), 1, 2, 5));
+			ChestGenHooks.addItem(ChestGenHooks.STRONGHOLD_CROSSING, new WeightedRandomChestContent(new ItemStack(this), 1, 2, 5));
+			ChestGenHooks.addItem(ChestGenHooks.DUNGEON_CHEST, new WeightedRandomChestContent(new ItemStack(this), 1, 2, 5));
+		}
 	}
 
 	@Override
