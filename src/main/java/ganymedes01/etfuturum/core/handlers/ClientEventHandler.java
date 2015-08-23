07f30165cc63401fc1fda9589b0f064ed4afcf63
ganymedes01 <foka_12@hotmail.com>
2015-08-23 01:07:49 +0100

    Fix blending issue with armours above level 128

--- src/main/java/ganymedes01/etfuturum/core/handlers/ClientEventHandler.java
+++ src/main/java/ganymedes01/etfuturum/core/handlers/ClientEventHandler.java
@@ -4,20 +4,27 @@ import ganymedes01.etfuturum.EtFuturum;
 import ganymedes01.etfuturum.client.OpenGLHelper;
 import net.minecraftforge.client.event.RenderPlayerEvent;
 import net.minecraftforge.client.event.RenderPlayerEvent.SetArmorModel;
+
+import org.lwjgl.opengl.GL11;
+
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 
 public class ClientEventHandler {
 
 	@SubscribeEvent
 	public void renderPlayerEventPre(RenderPlayerEvent.Pre event) {
-		if (EtFuturum.enableTransparentAmour)
+		if (EtFuturum.enableTransparentAmour) {
 			OpenGLHelper.enableBlend();
+			OpenGLHelper.blendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+		}
 	}
 
 	@SubscribeEvent
 	public void renderPlayerSetArmour(SetArmorModel event) {
-		if (EtFuturum.enableTransparentAmour)
+		if (EtFuturum.enableTransparentAmour) {
 			OpenGLHelper.enableBlend();
+			OpenGLHelper.blendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+		}
 	}
 
 	@SubscribeEvent
