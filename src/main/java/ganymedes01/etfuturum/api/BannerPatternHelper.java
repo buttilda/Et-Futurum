9d03e49d339f2688625e90c066d0d24183e2a2ce
ganymedes01 <foka_12@hotmail.com>
2015-08-25 01:49:16 +0100

    Made the API not depend on internal classes

--- src/main/java/ganymedes01/etfuturum/api/BannerPatternHelper.java
+++ src/main/java/ganymedes01/etfuturum/api/BannerPatternHelper.java
@@ -1,16 +1,17 @@
 package ganymedes01.etfuturum.api;
 
-import ganymedes01.etfuturum.tileentities.TileEntityBanner.EnumBannerPattern;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.util.EnumHelper;
 
 public class BannerPatternHelper {
 
-	public static void addPattern(String name, String id, ItemStack craftingItem) {
-		EnumHelper.addEnum(EnumBannerPattern.class, name.toUpperCase(), new Class[] { String.class, String.class, ItemStack.class }, new Object[] { name, id, craftingItem });
+	@SuppressWarnings("unchecked")
+	public static void addPattern(String name, String id, ItemStack craftingItem) throws ClassNotFoundException {
+		EnumHelper.addEnum((Class<Enum<?>>) Class.forName("ganymedes01.etfuturum.tileentities.TileEntityBanner$EnumBannerPattern"), name.toUpperCase(), new Class[] { String.class, String.class, ItemStack.class }, new Object[] { name, id, craftingItem });
 	}
 
-	public static void addPattern(String name, String id, String craftingTop, String craftingMid, String craftingBot) {
-		EnumHelper.addEnum(EnumBannerPattern.class, name.toUpperCase(), new Class[] { String.class, String.class, String.class, String.class, String.class }, new Object[] { name, id, craftingTop, craftingMid, craftingBot });
+	@SuppressWarnings("unchecked")
+	public static void addPattern(String name, String id, String craftingTop, String craftingMid, String craftingBot) throws ClassNotFoundException {
+		EnumHelper.addEnum((Class<Enum<?>>) Class.forName("ganymedes01.etfuturum.tileentities.TileEntityBanner$EnumBannerPattern"), name.toUpperCase(), new Class[] { String.class, String.class, String.class, String.class, String.class }, new Object[] { name, id, craftingTop, craftingMid, craftingBot });
 	}
 }
