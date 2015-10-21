6b1405722daf8baa440791f19b891e04ab73e1a4
ganymedes01 <foka_12@hotmail.com>
2015-10-21 10:32:19 +0100

    Hopefully fix crash with black hearts particles

--- src/main/java/ganymedes01/etfuturum/network/BlackHeartParticlesHandler.java
+++ src/main/java/ganymedes01/etfuturum/network/BlackHeartParticlesHandler.java
@@ -7,6 +7,8 @@ import net.minecraftforge.common.DimensionManager;
 import cpw.mods.fml.common.network.simpleimpl.IMessage;
 import cpw.mods.fml.common.network.simpleimpl.IMessageHandler;
 import cpw.mods.fml.common.network.simpleimpl.MessageContext;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 
 public class BlackHeartParticlesHandler implements IMessageHandler<BlackHeartParticlesMessage, IMessage> {
 
@@ -17,8 +19,13 @@ public class BlackHeartParticlesHandler implements IMessageHandler<BlackHeartPar
 		double y = message.y;
 		double z = message.z;
 
-		Minecraft.getMinecraft().effectRenderer.addEffect(new BlackHeartFX(world, x, y, z));
+		spawnParticles(world, x, y, z);
 
 		return null;
+	}
+
+	@SideOnly(Side.CLIENT)
+	private void spawnParticles(World world, double x, double y, double z) {
+		Minecraft.getMinecraft().effectRenderer.addEffect(new BlackHeartFX(world, x, y, z));
 	}
 }
