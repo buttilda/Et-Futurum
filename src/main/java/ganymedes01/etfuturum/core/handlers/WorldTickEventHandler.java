9f6242fbd4b3a9e8da4bd6f5dbd171feb19aff9f
ganymedes01 <foka_12@hotmail.com>
2015-11-09 14:31:57 +0000

    Closes #47

--- src/main/java/ganymedes01/etfuturum/core/handlers/WorldTickEventHandler.java
+++ src/main/java/ganymedes01/etfuturum/core/handlers/WorldTickEventHandler.java
@@ -1,26 +1,28 @@
 package ganymedes01.etfuturum.core.handlers;
 
-import ganymedes01.etfuturum.ModBlocks;
-
 import java.util.List;
 
-import net.minecraft.block.Block;
-import net.minecraft.init.Blocks;
-import net.minecraft.tileentity.TileEntity;
-import net.minecraft.world.World;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.TickEvent.Phase;
 import cpw.mods.fml.common.gameevent.TickEvent.WorldTickEvent;
 import cpw.mods.fml.relauncher.Side;
+import ganymedes01.etfuturum.ModBlocks;
+import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.World;
 
 public class WorldTickEventHandler {
+
+	private boolean isReplacing = false;
 
 	@SubscribeEvent
 	@SuppressWarnings("unchecked")
 	public void tick(WorldTickEvent event) {
-		if (event.side != Side.SERVER || event.phase != Phase.END)
+		if (event.side != Side.SERVER || event.phase != Phase.END || isReplacing)
 			return;
 
+		isReplacing = true;
 		World world = event.world;
 		for (TileEntity tile : (List<TileEntity>) world.loadedTileEntityList) {
 			Block block = world.getBlock(tile.xCoord, tile.yCoord, tile.zCoord);
@@ -29,5 +31,6 @@ public class WorldTickEventHandler {
 				return;
 			}
 		}
+		isReplacing = false;
 	}
 }
